d26d9770901ceece6bebf2f85e488a97df7f50e4
rafaelmbarroso <rafaelmbarroso@outlook.com>
2026-03-01 22:21:28 -0600

    New assignment and moved folder structure around

---
+++ assignments/Homework 2/assignment2/src/main.rs
@@ -0,0 +1,10 @@
+fn most_frequent_word(text: &srt) -> (String, usize) {
+
+    (max_word, max_count)
+}
+
+fn main() {
+    let text = "the quick brown fox jumps over the lazy dog the quick brown fox";
+    let (word, count) = most_frequest_word(text);
+    println!("Most frequest word: \"{}\" ({} times)", word, count);
+}
