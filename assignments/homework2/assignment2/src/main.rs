954b765107d8b1212d3dc10c6ebf01340143bebb
rafaelmbarroso <rafaelmbarroso@outlook.com>
2026-03-01 22:43:47 -0600

    Added Assignment 2

--- assignments/homework2/assignment2/src/main.rs
+++ assignments/homework2/assignment2/src/main.rs
@@ -1,3 +1,20 @@
+fn most_frequent_word(text: &str) -> (String, usize) {
+    
+    // Take a string and first split it up into words of which we can count the frequeny.
+    let mut max_word = String::new(); // Empty string to store the most frequent word
+    let mut max_count = 0; // Max word counter
+    for word in text.split_whitespace() {
+        let count = text.matches(word).count();
+        if count > max_count {
+            max_word = word.to_string();
+            max_count = count;
+        }
+    }
+    (max_word, max_count)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let text = "the quick brown fox jumps over the lazy dog the quick brown fox";
+    let (word, count) = most_frequent_word(text);
+    println!("Most frequent word: \"{}\" ({} times)", word, count);
 }
