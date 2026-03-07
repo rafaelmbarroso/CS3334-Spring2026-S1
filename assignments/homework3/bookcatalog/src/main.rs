1915743d6dd24dbe63e5d8e4fd15fd2f550b7a38
rafaelmbarroso <rafaelmbarroso@outlook.com>
2026-03-06 23:23:57 -0600

    Added homework 3

---
+++ assignments/homework3/bookcatalog/src/main.rs
@@ -0,0 +1,44 @@
+use std::fs::File;
+use std::io::{Write, BufReader, BufRead};
+
+struct Book {
+    title: String,
+    author: String,
+    year: u16,
+}
+
+fn save_books(books: &Vec<Book>, filename: &str) {
+    // TODO: Implement this function
+    // Hint: Use File::create() and write!() macro
+    
+    // Create the new file
+    File::create("books.txt");
+
+    for book in books {
+        Write!::(book);
+    }
+}
+
+/* 
+fn load_books(filename: &str) -> Vec<Book> {
+    // TODO: Implement this function
+    // Hint: Use File::open() and BufReader
+
+}
+    */
+
+fn main() {
+    let books = vec![
+        Book { title: "1984".to_string(), author: "George Orwell".to_string(), year: 1949 },
+        Book { title: "To Kill a Mockingbird".to_string(), author: "Harper Lee".to_string(), year: 1960 },
+    ];
+
+    save_books(&books, "books.txt");
+    println!("Books saved to file.");
+
+    let loaded_books = load_books("books.txt");
+    println!("Loaded books:");
+    for book in loaded_books {
+        println!("{} by {}, published in {}", book.title, book.author, book.year);
+    }
+}
