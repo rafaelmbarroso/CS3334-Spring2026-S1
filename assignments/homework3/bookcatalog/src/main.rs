a7e239f683e62ceab9777eb1f231eb67a20e69ca
rafaelmbarroso <rafaelmbarroso@outlook.com>
2026-03-06 23:58:40 -0600

    Updates to code

--- assignments/homework3/bookcatalog/src/main.rs
+++ assignments/homework3/bookcatalog/src/main.rs
@@ -12,20 +12,40 @@ fn save_books(books: &Vec<Book>, filename: &str) {
     // Hint: Use File::create() and write!() macro
     
     // Create the new file
-    File::create("books.txt");
+    let mut file = File::create(filename);
 
+    let writer = Write::write(&mut file, ""); // Clear the file before writing
+    // Save each book to the file per line and in the struct format
     for book in books {
-        Write!::(book);
+        writer!(file, "{}|{}|{}", book.title, book.author, book.year);
     }
 }
 
-/* 
+ 
 fn load_books(filename: &str) -> Vec<Book> {
     // TODO: Implement this function
     // Hint: Use File::open() and BufReader
+    // Open the file and read it line by line
+    // We also need to parse it into the struct format and return a vector of books
+    let file = File::open(filename).expect("Where's my file? Can't find books.txt");
+    let reader = BufReader::new(file);
+    let mut books = Vec::new();
 
+    // Read each line and parse it the Book struct
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let parts: Vec<&str> = line.split('|').collect();
+        if parts.len() == 3 {
+            let book = Book {
+                title: parts[0].to_string(),
+                author: parts[1].to_string(),
+                year: parts[2].parse().unwrap_or(0),
+            };
+            books.push(book);
+        }
+    }
+    books
 }
-    */
 
 fn main() {
     let books = vec![
